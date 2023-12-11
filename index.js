c973b05da001138bf069c31a570ada210f0a465d
naushinhoque <fnmh@yahoo.com>
2023-12-11 14:58:43 -0600

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // start your server here
+require('dotenv').config()
+
+const server = require('./api/server')
+
+const port = process.env.port 
+
+server.listen(port, () => console.log(`running on port ${port}`))
