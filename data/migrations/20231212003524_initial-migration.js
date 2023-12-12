889085eaa8efec9035f85bebcd5acc28eaf9155d
naushinhoque <fnmh@yahoo.com>
2023-12-11 18:57:27 -0600

    migrations

--- data/migrations/20231212003524_initial-migration.js
+++ data/migrations/20231212003524_initial-migration.js
@@ -5,16 +5,48 @@
 exports.up = async function(knex) {
   await knex.schema
     .createTable('projects', table => {
-        table.increments()
+        table.increments('project_id')
+        table.string('project_name').notNullable()
+        table.text('project_description')
+        table.boolean('project_completed').defaultTo(false)
     })
     .createTable('resources', table => {
-        table.increments()
+        table.increments('resource_id')
+        table.string('resource_name').notNullable().unique()
+        table.text('resource_description')
     })
     .createTable('tasks', table => {
-        table.increments()
+        table.increments('task_id')
+        table.text('task_description').notNullable()
+        table.text('task_notes')
+        table.boolean('task_completed').defaultTo(false)
+        table
+            .integer('project_id')
+            .notNullable()
+            .unsigned()
+            .references('project_id')
+            .inTable('projects')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE')
     })
     .createTable('project_resource', table => {
-        table.increments()
+        table.increments('project_resource_id')
+        table
+            .integer('project_id')
+            .notNullable()
+            .unsigned()
+            .references('project_id')
+            .inTable('projects')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE')
+        table
+            .integer('resource_id')
+            .notNullable()
+            .unsigned()
+            .references('resource_id')
+            .inTable('resources')
+            .onUpdate('CASCADE')
+            .onDelete('CASCADE')
     })
 };
 
